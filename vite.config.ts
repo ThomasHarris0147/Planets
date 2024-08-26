8bf899a0bc6baaba432f7a607434bce632f3082f
thegang_zThomasH <zthomash@pttep.com>
2024-08-26 13:29:59 +0700

    added deploy code

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: import.meta.env.MODE === 'production' ? '/planets/' : '/',
   build: {
     minify: false
   },
