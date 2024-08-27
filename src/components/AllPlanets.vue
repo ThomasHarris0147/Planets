6b5bee71d1c350509a8fe4f900340f42375fd8e1
thegang_zThomasH <zthomash@pttep.com>
2024-08-27 21:23:51 +0700

    fixed prd code

--- src/components/AllPlanets.vue
+++ src/components/AllPlanets.vue
@@ -5,18 +5,21 @@ import PlanetObject from './PlanetObject.vue'
 import { planets } from './PlanetConfig'
 import { usePlanetStore } from '../stores/PlanetStore'
 const planetStore = usePlanetStore()
-
-const allPlanets = ref(planets)
+const defaultPlanets = planets
 if (import.meta.env.VITE_APP_MODE == 'production') {
-  for (let planet in allPlanets.value) {
+  for (let planet in defaultPlanets) {
     // for some reason base: '/planets/' is not replacing the path here.
     // so Im doing it manually
-    allPlanets.value[planet].path = '/planets' + allPlanets.value[planet].path
+    defaultPlanets[planet].path = '/planets' + defaultPlanets[planet].path
+    if ('envPath' in defaultPlanets[planet]) {
+      defaultPlanets[planet].envPath = '/planets' + defaultPlanets[planet].envPath
+    }
   }
 }
-
+const allPlanets = ref(defaultPlanets)
+console.log(allPlanets.value)
 function turnOnAllPlanets() {
-  allPlanets.value = planets
+  allPlanets.value = defaultPlanets
 }
 function isolatePlanet(planetName) {
   for (let planet in allPlanets.value) {
